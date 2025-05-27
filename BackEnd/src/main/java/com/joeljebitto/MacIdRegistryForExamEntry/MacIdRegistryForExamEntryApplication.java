7935cc4a094e3f2ff8fc90d769a1a81c23213194
Joel Jebitto <joeljebitto@gmail.com>
2025-05-27 09:23:15 +0530

    BackEnd Project Created

---
+++ BackEnd/src/main/java/com/joeljebitto/MacIdRegistryForExamEntry/MacIdRegistryForExamEntryApplication.java
@@ -0,0 +1,13 @@
+package com.joeljebitto.MacIdRegistryForExamEntry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacIdRegistryForExamEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacIdRegistryForExamEntryApplication.class, args);
+	}
+
+}
