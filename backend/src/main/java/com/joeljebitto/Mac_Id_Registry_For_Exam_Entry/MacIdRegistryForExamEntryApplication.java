554d596d967a4d242f3ac7f5f622e7acda4bb9b3
Joel Jebitto <joeljebitto@gmail.com>
2025-06-02 22:22:27 +0530

    backend added

---
+++ backend/src/main/java/com/joeljebitto/Mac_Id_Registry_For_Exam_Entry/MacIdRegistryForExamEntryApplication.java
@@ -0,0 +1,13 @@
+package com.joeljebitto.Mac_Id_Registry_For_Exam_Entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacIdRegistryForExamEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacIdRegistryForExamEntryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joeljebitto/Mac_Id_Registry_For_Exam_Entry/MacIdRegistryForExamEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeljebitto.Mac_Id_Registry_For_Exam_Entry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacIdRegistryForExamEntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
